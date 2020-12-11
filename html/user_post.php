5d0b21a99d9fa01533e69523a19ff5b820a57875
Ivan Nushkov <nushkovivan@gmail.com>
2020-12-11 17:07:04 +0200

    User pic in posts updated.

--- html/user_post.php
+++ html/user_post.php
@@ -1,11 +1,18 @@
     <div id="post">
         <div>
-            <img src="../images/user1.jpg" style="width: 75px; margin-right: 4px;">
+            <?php
+                $image = "male.jpg";
+                if($row_user['gender'] == "female"){
+                    $image = "female.jpg";
+                }
+
+            ?>
+            <img src="<?php echo $image?>" style="width: 75px; margin-right: 4px;">
         </div>
         <div>
-            <div style="font-weight: bold; color: #405b9d;"><?php echo $row_user['first_name'] . "" . $row_user['last_name']; ?></div>
-            <?php echo $row['post'] ?>
+            <div style="font-weight: bold; color: #405b9d;"><?php echo $row_user['first_name'] . " " . $row_user['last_name']; ?></div>
+            <?php echo $row['post']; ?>
             <br><br>
-            <a href="">Like</a> . <a href="">Comment</a> . <span style="color: #999;">December 7 2020</span>
+            <a href="">Like</a> . <a href="">Comment</a> .<span style="color: #999;"><?php echo $row['date']; ?></span>
         </div>
     </div>
