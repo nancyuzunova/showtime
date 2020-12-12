13c9bf0eef8995ab371138fae8161c2cb107a6c9
Nancy Uzunova <nansy_98@abv.bg>
2020-12-12 21:22:43 +0200

    Showing user profile picture on their posts.

--- html/user_post.php
+++ html/user_post.php
@@ -1,33 +1,35 @@
 <div id="post">
     <div>
         <?php
-        $image = "../images/male.jpg";
-        if ($row_user['gender'] == "female") {
-            $image = "../images/female.jpg";
-        }
-
+            $image = "../images/male.jpg";
+            if ($row_user['gender'] == "female") {
+                $image = "../images/female.jpg";
+            }
+            if (file_exists($row_user['profile_image'])) {
+                $image = $editor->getThumbProfile($row_user['profile_image']);
+            }
         ?>
-        <img src="<?php echo $image ?>" style="width: 75px; margin-right: 4px;">
+        <img src="<?php echo $image ?>" style="width: 75px; margin-right: 10px; border-radius: 50%;">
     </div>
     <div style="width: 100%">
         <div style="font-weight: bold; color: #405b9d;">
             <?php
-                echo $row_user['first_name'] . " " . $row_user['last_name'];
-                if ($row['is_profile_image']){
-                    $pronoun = "his";
-                    if ($row_user['gender'] == "Female"){
-                        $pronoun = "her";
-                    }
-                    echo "<span style='font-weight: normal; color: gray'> updated $pronoun profile image/span>";
-                }
-            if ($row['is_cover_image']){
+            echo $row_user['first_name'] . " " . $row_user['last_name'];
+            if ($row['is_profile_image']) {
                 $pronoun = "his";
-                if ($row_user['gender'] == "Female"){
+                if ($row_user['gender'] == "Female") {
+                    $pronoun = "her";
+                }
+                echo "<span style='font-weight: normal; color: gray'> updated $pronoun profile image/span>";
+            }
+            if ($row['is_cover_image']) {
+                $pronoun = "his";
+                if ($row_user['gender'] == "Female") {
                     $pronoun = "her";
                 }
                 echo "<span style='font-weight: normal; color: gray'> updated $pronoun cover image/span>";
             }
-             ?>
+            ?>
         </div>
         <?php echo $row['post']; ?>
         <br><br>
