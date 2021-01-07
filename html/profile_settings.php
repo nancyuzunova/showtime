c18cc9933579c920f8eaa7432b390f8ac1415ff0
Nancy Uzunova <nansy_98@abv.bg>
2021-01-07 17:13:39 +0200

    Added change password button.

--- html/profile_settings.php
+++ html/profile_settings.php
@@ -15,11 +15,10 @@
                     echo "<input type='text' id='textbox' name='firstName' value='". htmlspecialchars($settings['first_name'])."' placeholder='First name' >";
                     echo "<input type='text' id='textbox' name='lastName' value='". htmlspecialchars($settings['last_name'])."' placeholder='Last name' >";
                     echo "<input type='text' id='textbox' name='email' value='". htmlspecialchars($settings['email'])."' placeholder='Email' >";
-                    echo "<input type='password' id='textbox' name='password' placeholder='Password' value='". htmlspecialchars($settings['password'])."'>";
-                    echo "<input type='password' id='textbox' name='password2' placeholder='Password' value='". htmlspecialchars($settings['password'])."'>";
                     echo "<br>About me:<br>
                             <textarea id='textbox' style='height: 200px;' name='about'>".htmlspecialchars($settings['about'])."</textarea>";
-                    echo '<input id="postButton" type="submit" value="save">';
+                    echo '<input id="postButton" type="submit" value="Save">';
+                    echo '<input id="changePassword" type="submit" value="Change password">';
                 }
             ?>
         </form>
